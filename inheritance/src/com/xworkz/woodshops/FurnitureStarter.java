8e6051a173040bb4a46c236b821ceadc79035fac
SahanaGP <sahanagp.xworkz@gmail.com>
2021-11-08 22:53:39 +0530

    inheritance examples

--- inheritance/src/com/xworkz/woodshops/FurnitureStarter.java
+++ inheritance/src/com/xworkz/woodshops/FurnitureStarter.java
@@ -9,7 +9,7 @@ public class FurnitureStarter {
 	
 		ComputerTable computerTable=new ComputerTable("Hexagon",8000.00f,2);
 		System.out.println(computerTable.name);
-		//System.out.println(computerTable.price);
+		System.out.println(computerTable.quantity);
 		
 		DineTable dine=new DineTable("Standard");
 		System.out.println(dine.name);
